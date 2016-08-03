1316c8dd2c44fb38e574fa1d1ff52a6a599f8146
Diego Ramos <rdiego26@gmail.com>
2016-08-03 18:04:21 -0300

    ADD more user API tests .
    - should signin returns user created
    - should search user with token

--- test/api/user.js
+++ test/api/user.js
@@ -16,6 +16,11 @@ describe('User API', function() {
     phones: [{phone: '123456789', ddd: '21'}]
   };
 
+  const _signinData = {
+    email: "email@email.com",
+    password: "12345"
+  };
+
   afterEach(function(done) {
 
     userDao.findOne({email: _validUser.email}).then(function(_userFetched) {
@@ -50,12 +55,80 @@ describe('User API', function() {
           if(!err) {
             var _createdUser = res.body;
 
-            assert.isArray(_createdUser.phones);
-            assert.equal(1, _createdUser.phones.length);
-
-            assert.ok(_validUser.cpf === _createdUser.cpf);
+            assert.ok(_validUser.email === _createdUser.email);
             done();
 
+          }
+
+        });
+
+  });
+
+  it('should signin returns user created', function(done) {
+
+
+    request(app)
+        .post('/api/user')
+        .send(_validUser)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+
+          request(app)
+              .post('/api/user/signin')
+              .send(_signinData)
+              .expect('Content-Type', /json/)
+              .expect(200)
+              .end(function(err, res) {
+
+                if(!err) {
+                  var _createdUser = res.body;
+
+                  assert.ok(_signinData.email === _createdUser.email);
+                  assert.property(_createdUser, 'token');
+
+                  done();
+
+                }
+
+              });
+
+        });
+
+
+
+  });
+
+  it('should search user with token', function(done) {
+
+    request(app)
+        .post('/api/user')
+        .send(_validUser)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function(err, res) {
+
+          if (!err) {
+
+            var _createdUser = res.body;
+
+            request(app)
+                .get('/api/user/' + _createdUser.id)
+                .expect('Content-Type', /json/)
+                .set('Authorization', _createdUser.token)
+                .expect(200)
+                .end(function(err, res) {
+
+                  if(!err) {
+                    var _fetchedUser = res.body;
+
+                    assert.ok(_fetchedUser.token === _createdUser.token);
+                    assert.property(_fetchedUser, 'token');
+
+                    done();
+
+                  }
+                });
           }
 
         });
